3202d598a9745b07e978f61070971c92757f98fc
cothi <jiungdev@gmail.com>
2022-09-26 23:08:17 +0900

    refac: Eliminate unnecessary functions for complexity and modify code for themlsp config

--- lua/sidebar-nvim/builtin/files.lua
+++ lua/sidebar-nvim/builtin/files.lua
@@ -206,14 +206,6 @@ local function copy_file(src, dest, confirm_overwrite)
     end)
 end
 
-local function get_length(iter)
-    local length = 0
-    for _ in iter do
-        length = length + 1
-    end
-    return length
-end
-
 local function create_file(dest)
     if luv.fs_access(dest, "r") ~= false then
         vim.schedule(function()
@@ -221,15 +213,17 @@ local function create_file(dest)
         end)
         return
     end
+
     local is_file = not dest:match("/" .. "$")
-    local last_path_length = get_length(utils.path_split(dest))
-    local now_length = 0
+    local parent_folders = vim.fn.fnamemodify(dest, ":h")
     local now_check_path = ""
+    local now_check_path_parent = ""
 
     for path in utils.path_split(dest) do
-        now_length = now_length + 1
         now_check_path = now_check_path .. path
-        if is_file and now_length == last_path_length then
+        now_check_path_parent = vim.fn.fnamemodify(now_check_path, ":h")
+
+        if is_file and now_check_path_parent == parent_folders then
             luv.fs_open(dest, "w", 420, function(err, file)
                 if err ~= nil then
                     vim.schedule(function()
@@ -242,7 +236,7 @@ local function create_file(dest)
         else if not utils.file_exist(now_check_path) then
                 local success = luv.fs_mkdir(now_check_path, 493)
                 if not success then
-                    utils.echo_warning('Could not create directory "' .. now_check_path)
+                    utils.echo_warning('Could not create directory ' .. now_check_path)
                 end
             end
         end
